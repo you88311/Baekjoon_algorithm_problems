4ab1b52b196c81a4642f2d169e7f25ad8d73c525
YoungEun OH <you88311@naver.com>
2022-07-24 20:26:48 +0900

    [Silver I] Title: 그림, Time: 572 ms, Memory: 91792 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -4,28 +4,46 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static boolean isInRange(Integer[] coordinate, int[][] arr) {
+    final Integer[] X = {1, 0, -1, 0};
+    final Integer[] Y = {0, 1, 0, -1};
+    public int bfs(Queue<Integer[]>queue,int[][] arr, boolean[][] visited, Integer[] cord) {
+        queue.add(cord);
+        visited[cord[0]][cord[1]] = true;
+        int graphSize = 1;
+
+        while (!queue.isEmpty()) {
+            cord = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                Integer[] adjCord = {cord[0] + X[i], cord[1] + Y[i]};
+                if (!isInRange(adjCord, arr)) continue;
+                if (!visited[adjCord[0]][adjCord[1]] && arr[adjCord[0]][adjCord[1]] == 1) {
+                    queue.add(adjCord);
+                    visited[adjCord[0]][adjCord[1]] = true;
+                    graphSize++;
+                }
+            }
+        }
+
+        return graphSize;
+    }
+
+    private boolean isInRange(Integer[] coordinate, int[][] arr) {
         return coordinate[0] < arr.length && coordinate[1] < arr[0].length && coordinate[0] >= 0 && coordinate[1] >= 0;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         Main main = new Main();
-        Queue<Integer[]> queue = new LinkedList<>();
         String[] size = br.readLine().split(" ");
         final int COLUMN = Integer.parseInt(size[0]);
         final int ROW = Integer.parseInt(size[1]);
-        final Integer[] X = {1, 0, -1, 0};
-        final Integer[] Y = {0, 1, 0, -1};
         int[][] arr = new int[COLUMN][ROW];
         boolean[][] visited = new boolean[COLUMN][ROW];//기본값 false로 초기화
-        Integer[] cord = new Integer[2];
-
+        //상하좌우 좌표
         int graphCnt = 0;
         int maxGraphSize = 0;
         int graphSize = 0;
+        Queue<Integer[]> queue = new LinkedList<>();
 
         /*입력 값 그래프에 저장*/
         for (int i = 0; i < COLUMN; i++) {
@@ -38,25 +56,7 @@ public class Main {
         for (int i = 0; i < COLUMN; i++) {
             for (int j = 0; j < ROW; j++) {
                 if (arr[i][j] == 0 || visited[i][j]) continue;
-                cord[0] = i;
-                cord[1] = j;
-                queue.add(cord);
-                visited[cord[0]][cord[1]] = true;
-                graphSize = 1;
-
-                while (!queue.isEmpty()) {
-                    cord = queue.poll();
-
-                    for (int k = 0; k < 4; k++) {
-                        Integer[] adjCord = {cord[0] + X[k], cord[1] + Y[k]};
-                        if (!isInRange(adjCord, arr)) continue;
-                        if (!visited[adjCord[0]][adjCord[1]] && arr[adjCord[0]][adjCord[1]] == 1) {
-                            queue.add(adjCord);
-                            visited[adjCord[0]][adjCord[1]] = true;
-                            graphSize++;
-                        }
-                    }
-                }
+                graphSize = main.bfs(queue,arr, visited, new Integer[]{i, j});
                 graphCnt++;
                 maxGraphSize = Math.max(maxGraphSize, graphSize);
             }
@@ -64,6 +64,7 @@ public class Main {
 
         System.out.println(graphCnt);
         System.out.print(maxGraphSize);
+        br.close();
     }
 }
 
