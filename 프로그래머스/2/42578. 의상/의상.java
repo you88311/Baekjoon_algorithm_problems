01b45d82d311463392e0982a073dc72d91291259
YoungEun OH <you88311@naver.com>
2023-11-14 19:59:33 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        // <의상의 종류, 개수> 쌍의 map 생성
+        for(int i = 0; i < clothes.length; i++){
+            String clothType = clothes[i][1];
+            Integer cnt;
+            
+            if(map.containsKey(clothType)){
+                cnt = map.get(clothType)+1;
+            }else{
+                cnt = new Integer(1);
+            }
+            map.put(clothType, cnt);
+        }
+        
+        // 경우의 수 계산
+        int result = 0;
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            int currentCnt = entry.getValue()+1;
+            if(result == 0){
+                result = currentCnt;
+            }else{
+                result *= currentCnt;
+            }
+        }
+        result -= 1; // 모든 의상의 종류에 대해 입지 않은 경우의 수 제거
+        
+        return result;
+    }
+}
