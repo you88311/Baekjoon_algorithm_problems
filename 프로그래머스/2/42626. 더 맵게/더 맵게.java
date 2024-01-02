59a8505a81c556bf2aa7f58c3889cff41de095b6
YoungEun OH <you88311@naver.com>
2024-01-02 21:41:07 +0900

    [level 2] Title: 더 맵게, Time: 1354.68 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 // 이거는 꼭 다시 풀어봐
+// 내가 구현한 더러운?while문보다 깔끔한 답변 있음
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
@@ -12,28 +13,36 @@ class Solution {
         for(int e : scoville){
             priorityQueue.add(e);
         }
+
+        // 최초소스
+//           while(!priorityQueue.isEmpty()){
+//             firstScoville = priorityQueue.poll();
+//             if(firstScoville.intValue() >= K){
+//                 return answer;
+//             }
+            
+//             if(priorityQueue.isEmpty()){
+//                 return -1;
+//             }
+            
+//             secondScoville =  priorityQueue.poll();
+//             answer++;
+            
+//             newScoville = firstScoville  + secondScoville * 2;   
+            
+//             priorityQueue.add(newScoville);
+            
+//         }
         
-        
-        while(!priorityQueue.isEmpty()){
+        while(priorityQueue.size() > 1 && priorityQueue.peek() < K){
             firstScoville = priorityQueue.poll();
-            if(firstScoville.intValue() >= K){
-                return answer;
-            }
-            
-            if(priorityQueue.isEmpty()){
-                return -1;
-            }
-            
             secondScoville =  priorityQueue.poll();
-            answer++;
-            
             newScoville = firstScoville  + secondScoville * 2;   
             
             priorityQueue.add(newScoville);
-            
+            answer++;
         }
         
-        
-        return -1;
+        return priorityQueue.peek() >= K ? answer : -1;
     }
 }
