6b63ca362eb9f9f7beeb0c86138252644b8004d2
YoungEun OH <you88311@naver.com>
2024-01-16 19:58:11 +0900

    [level 2] Title: 더 맵게, Time: 1552.29 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,48 +1,31 @@
 import java.util.*;
 
-// 이거는 꼭 다시 풀어봐
-// 내가 구현한 더러운?while문보다 깔끔한 답변 있음
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        Integer firstScoville;
-        Integer secondScoville;
-        Integer newScoville;
+        
+        List<Integer> list = new ArrayList<>();
+        for(int e : scoville){
+           list.add(e); 
+        }
         
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        for(int e : scoville){
-            priorityQueue.add(e);
-        }
-
-        // 최초소스
-//           while(!priorityQueue.isEmpty()){
-//             firstScoville = priorityQueue.poll();
-//             if(firstScoville.intValue() >= K){
-//                 return answer;
-//             }
-            
-//             if(priorityQueue.isEmpty()){
-//                 return -1;
-//             }
-            
-//             secondScoville =  priorityQueue.poll();
-//             answer++;
-            
-//             newScoville = firstScoville  + secondScoville * 2;   
-            
-//             priorityQueue.add(newScoville);
-            
-//         }
+        priorityQueue.addAll(list);
         
-        while(priorityQueue.size() > 1 && priorityQueue.peek() < K){
-            firstScoville = priorityQueue.poll();
-            secondScoville =  priorityQueue.poll();
-            newScoville = firstScoville  + secondScoville * 2;   
+        int prev = 0;
+        int current = 0;
+        int mixed = 0;
+        while(priorityQueue.size() >=2 && priorityQueue.peek() < K){
             
-            priorityQueue.add(newScoville);
+            prev = priorityQueue.poll();
+            current = priorityQueue.poll();
+            mixed =  prev + current*2 ;  
+            
+            priorityQueue.add(mixed);
+            
             answer++;
         }
         
-        return priorityQueue.peek() >= K ? answer : -1;
+        return priorityQueue.peek() < K ? -1 : answer;
     }
 }
